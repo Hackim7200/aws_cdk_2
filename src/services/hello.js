d215b4fe3d64b4ce9b4d59467b359e915d984508
Hikmatullah <hikmathakim7200@gmail.com>
2025-09-28 11:57:51 +0100

    using nodjs lambda to write ts and bundling external library using esbundle

--- src/services/hello.js
+++
@@ -1,6 +0,0 @@
-exports.main = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello! i will read from the table " + process.env.TABLE_NAME }),
-  };
-};
---
+++ src/services/hello.ts
@@ -0,0 +1,16 @@
+import {
+  APIGatewayProxyEvent,
+  Context,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import { v4 } from "uuid";
+async function handler(event: APIGatewayProxyEvent, context: Context) {
+  const response: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: JSON.stringify("hellow from lambda" + v4()),
+  };
+  console.log(event);
+
+  return response;
+}
+export { handler };
