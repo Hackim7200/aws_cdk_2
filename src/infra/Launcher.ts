0ddcc1c9c71bedc35c658657e8bb95851e36cad2
Hikmatullah <hikmathakim7200@gmail.com>
2025-09-28 10:49:30 +0100

    lambda functions

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,7 +1,9 @@
 import { App } from "aws-cdk-lib";
 import { DataStack } from "./stack/DataStack";
+import { LambdaStack } from "./stack/LambdaStack";
 
 
 
 const app = new App();
 new DataStack(app, "DataStack");
+new LambdaStack(app, "LambdaStack");
