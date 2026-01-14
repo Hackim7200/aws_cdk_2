0b5b498c8df64720d7e8d229cc3d8e020da173df
Hikmatullah <hikmathakim7200@gmail.com>
2026-01-14 13:32:47 +0000

    Cors headers configurations

--- src/infra/stacks/ApiStack.ts
+++ src/infra/stacks/ApiStack.ts
@@ -1,5 +1,13 @@
 import { Stack, StackProps } from "aws-cdk-lib";
-import { AuthorizationType, CognitoUserPoolsAuthorizer, Cors, LambdaIntegration, MethodOptions, ResourceOptions, RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  Cors,
+  LambdaIntegration,
+  MethodOptions,
+  ResourceOptions,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
 import { AllowedMethods } from "aws-cdk-lib/aws-cloudfront";
 import { IUserPool } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
@@ -14,15 +22,20 @@ export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props);
 
-
     const api = new RestApi(this, "SpacesApi"); //create a rest api named spacesApi
 
-    const autherizer = new CognitoUserPoolsAuthorizer(this,"SpacesApiAuthorizer", {cognitoUserPools:[props.userPool],
-      identitySource: "method.request.header.Authorization",
-    });
+    const autherizer = new CognitoUserPoolsAuthorizer(
+      this,
+      "SpacesApiAuthorizer",
+      {
+        cognitoUserPools: [props.userPool],
+        identitySource: "method.request.header.Authorization",
+      }
+    );
     autherizer._attachToApi(api);
 
-    const optionsWithAuth: MethodOptions = { // when this is passed with the lambda integration it will automatically authenticate the request befire responding
+    const optionsWithAuth: MethodOptions = {
+      // when this is passed with the lambda integration it will automatically authenticate the request befire responding
       authorizationType: AuthorizationType.COGNITO,
       authorizer: {
         authorizerId: autherizer.authorizerId,
@@ -30,19 +43,35 @@ export class ApiStack extends Stack {
     };
 
     const optionsWithCors: ResourceOptions = {
-      defaultCorsPreflightOptions:{
-        allowOrigins:Cors.ALL_ORIGINS,
-        allowMethods:Cors.ALL_METHODS
-      }
-    }
-    const spacesResource = api.root.addResource("reservation",optionsWithCors); // adds the path /reservaitons to the api gateway
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+    };
+    const spacesResource = api.root.addResource("reservation", optionsWithCors); // adds the path /reservaitons to the api gateway
     spacesResource.addMethod("GET", props.reservationLambdaIntegration); // adds a GET method to the spaces path
     spacesResource.addMethod("POST", props.reservationLambdaIntegration); // adds a POST method to the spaces path
 
-    const moviesResource = api.root.addResource("movies",optionsWithCors );
-    moviesResource.addMethod("GET", props.moviesLambdaIntegration,optionsWithAuth);
-    moviesResource.addMethod("POST", props.moviesLambdaIntegration,optionsWithAuth);
-    moviesResource.addMethod("PUT", props.moviesLambdaIntegration,optionsWithAuth);
-    moviesResource.addMethod("DELETE", props.moviesLambdaIntegration,optionsWithAuth);
+    const moviesResource = api.root.addResource("movies", optionsWithCors);
+    moviesResource.addMethod(
+      "GET",
+      props.moviesLambdaIntegration,
+      optionsWithAuth
+    );
+    moviesResource.addMethod(
+      "POST",
+      props.moviesLambdaIntegration,
+      optionsWithAuth
+    );
+    moviesResource.addMethod(
+      "PUT",
+      props.moviesLambdaIntegration,
+      optionsWithAuth
+    );
+    moviesResource.addMethod(
+      "DELETE",
+      props.moviesLambdaIntegration,
+      optionsWithAuth
+    );
   }
 }
